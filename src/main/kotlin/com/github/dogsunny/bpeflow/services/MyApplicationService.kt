cb5abfb41f5bd02b8ddb8899076dfc449425f2c5
GitHub Action <action@github.com>
2021-08-05 09:04:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/dogsunny/bpeflow/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dogsunny/bpeflow/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dogsunny.bpeflow.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dogsunny.bpeflow.MyBundle
 
 class MyApplicationService {
 
