50dc0b7b7f810b2d094f593e52b90dd7d00a9fd7
wenguoyin <wenguoyin@gmm01.com>
2021-08-05 18:12:27 +0800

    代理over

--- src/main/kotlin/com/github/dogsunny/bpeflow/language/FlowFileType.kt
+++ src/main/kotlin/com/github/dogsunny/bpeflow/language/FlowFileType.kt
@@ -1,6 +1,7 @@
 package com.github.dogsunny.bpeflow.language
 
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 class FlowFileType: LanguageFileType(FlowLanguage.INSTANCE) {
@@ -14,5 +15,5 @@ class FlowFileType: LanguageFileType(FlowLanguage.INSTANCE) {
 
     override fun getDefaultExtension() = "flow"
 
-    override fun getIcon(): Icon? = null
+    override fun getIcon(): Icon? = IconLoader.getIcon("/icon/file_icon.svg")
 }
